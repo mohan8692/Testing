d32813886fece1fa4f95a722ff6b082cf809f21e
mohan8692 <mohanmadaparthi123@gmail.com>
2020-01-09 16:12:11 +0530

    chnages in indes js file

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-.
+var spinner = '<div class="row"><div class="col-md-12 col-xs-12 col-sm-12"><div class="spinner"><div class="dot1"></div><div class="dot2"></div></div></div></div>';
+var globalDistrictLatLagArr=[];
+var globalOverviewResult;
+var customStartDate =  moment().subtract(20, 'years').startOf('year').format("DD/MM/YYYY");
+var customEndDate = moment().format('DD/MM/YYYY');
